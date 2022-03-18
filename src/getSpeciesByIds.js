64d1519eb4ba3a758532e8020111541e4975c2a9
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-18 10:08:51 -0300

    Implementada funçao getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+/* Usei duas Higher Order function, o método map para mapear os elementos
+do array sem modificar o array original e o find para retornar o valor do
+primeiro elemento do array que satisfizer a função */
 
+function getSpeciesByIds(...ids) {
+  if (!ids) return [];
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
+}
 module.exports = getSpeciesByIds;
