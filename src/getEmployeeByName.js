e20884dae7e150a45590f7465f8f0cdc67b11f5b
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-18 12:27:46 -0300

    implementada funçao getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
+/* referencia do metodo includes :
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes  */
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  const namesEmploeys = employees.filter((employee) => employeeName.includes(employee.firstName)
+  || employeeName.includes(employee.lastName));
+  return namesEmploeys.reduce(() => employeeName);
 }
-
 module.exports = getEmployeeByName;
