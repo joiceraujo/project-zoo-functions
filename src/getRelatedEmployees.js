794206b63b692963bc32d324053f732cc0531abc
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-18 16:38:37 -0300

    implementadas funçoes getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const managers = data.employees.filter((employee) => employee.managers.includes(managerId));
+  return managers.map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
-
 module.exports = { isManager, getRelatedEmployees };
