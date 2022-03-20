19d1553bcbca85b2bdc92ba16b82e0be4d553bfb
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-20 13:04:04 -0300

    implementada funçao countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const amountAnimals = {};
+  const { species } = data;
+  if (!animal) {
+    species.forEach((amount) => {
+      amountAnimals[`${amount.name}`] = amount.residents.length;
+    });
+    return amountAnimals;
+  }
+  const amountSpecie = species.filter((specie) => specie.name === animal.specie)[0].residents;
+  const amountSex = amountSpecie.filter((namedAnimals) => namedAnimals.sex === animal.sex);
+  return !animal.sex ? amountSpecie.length : amountSex.length;
 }
-
 module.exports = countAnimals;
