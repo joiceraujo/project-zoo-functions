7afb01c0f6c1756745dd4a3a143e9b80c97efbd5
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-20 20:31:41 -0300

    implementada funçao getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimal = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const findsAnimal = data.species.find((animal) => animal.id === firstAnimal)
+    .residents.reduce((acc, curr) => (curr.age > acc.age ? curr : acc));
+  return [findsAnimal.name, findsAnimal.sex, findsAnimal.age];
 }
-
 module.exports = getOldestFromFirstSpecies;
