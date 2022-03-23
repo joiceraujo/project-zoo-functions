d0d5857ecd0e5b56190fdc54df4c5a2e6149182a
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-22 21:42:12 -0300

    implementada funçao calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const adult = entrants.filter((entrants1) => entrants1.age >= 18 && entrants1.age < 50).length;
+  const child = entrants.filter((entrants1) => entrants1.age < 18).length;
+  const senior = entrants.filter((entrants1) => entrants1.age >= 50).length;
+  return { adult, child, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
-}
+  if (entrants === undefined) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
 
+  const adult = entrants.filter((entrants2) => entrants2.age >= 18 && entrants2.age < 50).length;
+  const child = entrants.filter((entrants2) => entrants2.age < 18).length;
+  const senior = entrants.filter((entrants2) => entrants2.age >= 50).length;
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
+}
 module.exports = { calculateEntry, countEntrants };
